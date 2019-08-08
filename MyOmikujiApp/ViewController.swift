2ce02703bee48ee1121d83ed665c8b4322d362c1
Hironao Uehara <[eurekahironao@eis.hokudai.ac.jp]>
2019-08-08 15:53:31 +0900

    last commit

--- MyOmikujiApp/ViewController.swift
+++ MyOmikujiApp/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func getOmikuji(_ sender: Any) {
+        let results = ["大吉", "吉", "中吉", "凶", "大凶"]
+        let random = arc4random_uniform(UInt32(results.count))
+        self.myLabel.text = results[Int(random)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.layer.cornerRadius = myLabel.bounds.width / 2
+        myLabel.layer.masksToBounds = true
     }
 
 
